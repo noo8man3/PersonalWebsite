b9bd0e0bdce69169ce0808022e5ef7f8fe79c3e5
noo8man3 <brendank196@gmail.com>
2023-01-22 15:05:36 -0500

    Added Event Delegation and got rid of that dopey table.

--- portfolio_index.js
+++ portfolio_index.js
@@ -11,44 +11,44 @@ const scriptsText = document.querySelector('.scripts-proj');
 const resumeBtn = document.querySelector(".resume-btn");
 const resumeText = document.querySelector(".resume-text");
 
-//could use Event Delegation here, but...
-desktopBtn.addEventListener('click', function(e) {
-    if(document.querySelector('.desktop-proj.hidden')) {
-        document.querySelector('.desktop-proj.hidden').classList.remove('hidden');
-        webText.classList.add('hidden');
-        mobileText.classList.add('hidden');
-        scriptsText.classList.add('hidden');
-    }
-});
+const btnRow = document.querySelector(".button-row");
 
-webBtn.addEventListener('click', function(e) {
-    if(document.querySelector('.web-proj.hidden')) {
-        document.querySelector('.web-proj.hidden').classList.remove('hidden');
-        desktopText.classList.add('hidden');
-        mobileText.classList.add('hidden');
-        scriptsText.classList.add('hidden');
-    }
-});
+const blinkSpans = document.querySelectorAll(".blink");
 
-mobileBtn.addEventListener('click', function(e) {
-    if(document.querySelector('.mobile-proj.hidden')) {
-        document.querySelector('.mobile-proj.hidden').classList.remove('hidden');
-        desktopText.classList.add('hidden');
-        webText.classList.add('hidden');
-        scriptsText.classList.add('hidden');
-    }
+btnRow.childNodes.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        let btn = e.currentTarget;
+        
+        if(btn == desktopBtn) {
+            desktopText.classList.remove('hidden');
+            webText.classList.add('hidden');
+            mobileText.classList.add('hidden');
+            scriptsText.classList.add('hidden');
+        }
+        else if(btn == webBtn) {
+            webText.classList.remove('hidden');
+            desktopText.classList.add('hidden');
+            mobileText.classList.add('hidden');
+            scriptsText.classList.add('hidden');
+        }
+        else if(btn == mobileBtn) {
+            mobileText.classList.remove('hidden');
+            desktopText.classList.add('hidden');
+            webText.classList.add('hidden');
+            scriptsText.classList.add('hidden');
+        }
+        else if(btn == scriptsBtn) {
+            scriptsText.classList.remove('hidden');
+            desktopText.classList.add('hidden');
+            webText.classList.add('hidden');
+            mobileText.classList.add('hidden');
+        }
+        else {
+            console.log("This shouldn't happen, but it did");
+        }
+    });
 });
-
-scriptsBtn.addEventListener('click', function(e) {
-    if(document.querySelector('.scripts-proj.hidden')) {
-        document.querySelector('.scripts-proj.hidden').classList.remove('hidden');
-        desktopText.classList.add('hidden');
-        webText.classList.add('hidden');
-        mobileText.classList.add('hidden');
-    }
-});
-
 
 resumeBtn.addEventListener("click", function (e) {
-    resumeText.classList.toggle("show-text");
+    resumeText.classList.toggle("hidden");
 });
